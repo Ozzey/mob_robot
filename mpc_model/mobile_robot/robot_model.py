e377206e53ca8c4c073b007a4f99e7193cd9fec2
Ozzey1 <adityanarendra5@gmail.com>
2024-01-07 03:15:11 +0300

    Commit 3: Rewrote dynamics
    *Before major changes*
    
    # need to debug
    # need to add obstacle cost

--- mpc_model/mobile_robot/robot_model.py
+++ mpc_model/mobile_robot/robot_model.py
@@ -1,6 +1,5 @@
 import casadi as ca
 from acados_template import AcadosModel
-import numpy as np
 
 
 def mobile_robot_model():
@@ -16,6 +15,11 @@ def mobile_robot_model():
     v = ca.MX.sym('v')
     theta = ca.MX.sym('theta')
 
+    x_dot = ca.MX.sym('x_dot')
+    y_dot = ca.MX.sym('y_dot')
+    theta_dot = ca.MX.sym('theta_dot')
+    v_dot = ca.MX.sym('v_dot')
+
     # Control
     a = ca.MX.sym('a')  # acceleration
     w = ca.MX.sym('w')  # angular velocity
@@ -23,23 +27,19 @@ def mobile_robot_model():
     # Define state and control vectors
     states = ca.vertcat(x, y, v, theta)
     controls = ca.vertcat(a, w)
-    rhs = [v*ca.cos(theta), v*ca.sin(theta), a, w]
-    x_dot = ca.MX.sym('x_dot', len(rhs))
 
-    # Create a CasADi function for the continuous-time dynamics
-    continuous_dynamics = ca.Function(
-        'continuous_dynamics',
-        [states, controls],
-        [ca.vcat(rhs)],
-        ["state", "control_input"],
-        ["rhs"],
-    )
+    rhs = ca.vertcat(states[2] * ca.cos(states[3]),
+                     states[2] * ca.sin(states[3]),
+                     controls[0],
+                     controls[1])
 
-    f_impl = x_dot - continuous_dynamics(states, controls)
+    x_dot = ca.vertcat(x_dot, y_dot, v_dot, theta_dot)
+
+    f_impl = x_dot - rhs
 
     model = AcadosModel()
 
-    model.f_expl_expr = continuous_dynamics(states, controls)
+    model.f_expl_expr = rhs
     model.f_impl_expr = f_impl
     model.x = states
     model.xdot = x_dot
